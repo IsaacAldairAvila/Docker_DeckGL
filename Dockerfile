14beaed9d5503980348d5234a2ae4b65151b19f0
Isaac Avila <isaacaldairavila@gmail.com>
2022-06-28 12:25:52 -0500

    mapa basico

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+# build env
+FROM node:16.14.0-alpine as build
+ENV TZ America/Bogota
+WORKDIR /app
+ENV PATH /app/node_modules/.bin:$PATH
+COPY package.json ./
+COPY package-lock.json ./
+
+RUN npm ci
+RUN npm install 
+
+RUN apk add nano
+COPY . ./
+
+RUN npm run build
+
+# production env
+FROM nginx:1.21.6
+COPY --from=build /app/build /usr/share/nginx/html
+COPY default.conf /etc/nginx/conf.d/default.conf
+EXPOSE 80
+CMD ["nginx", "-g", "daemon off;"]
--- src/App.js
+++ src/App.js
@@ -1,25 +1,118 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import DeckGL from '@deck.gl/react';
+import { _MapContext as MapContext, StaticMap } from 'react-map-gl';
+import { ScatterplotLayer } from '@deck.gl/layers';
 
 function App() {
+  const [data, setData] = useState([
+    { name: 'b', opacity: 0.8, exits: 100, coordinates: [-122.41669, 37.791], color: [255, 116, 0], angle: 0 },
+    { name: 'b', opacity: 0.8, exits: 100, coordinates: [-122.41669, 37.781], color: [220, 255, 0], angle: 90 },
+    { name: 'b', opacity: 0.8, exits: 100, coordinates: [-122.41669, 37.771], color: [62, 255, 0] , angle: 0 },
+    { name: 'b', opacity: 0.8, exits: 100, coordinates: [-122.40769, 37.781], color: [0, 255, 174], angle: 90 },
+    { name: 'b', opacity: 0.8, exits: 100, coordinates: [-122.43869, 37.781], color: [0, 158, 255], angle: 0 },
+    { name: 'b', opacity: 0.8, exits: 100, coordinates: [-122.42569, 37.781], color: [197, 0, 255], angle: 90 },
+  ])
+  const viewState = {
+    longitude: -122.41669,
+    latitude: 37.7853,
+    zoom: 13,
+    pitch: 0,
+    bearing: 0
+  }
+
+  const [ani, setAni] = useState(true)
+  const cambio = () => {
+    setData(data.map(x => ({
+      ...x,
+      exits: cambiotam(x.exits, x.name)
+    })))
+  }
+
+  const cambiotam = (x, type) => {
+    if (type === 'a') {
+      return 1000
+    }
+    else {
+      if (ani) {
+        if (x >= 1000) {
+          setAni(!ani)
+          return x - 10
+        }
+        else {
+          return x + 10
+        }
+      }
+      else {
+        if (x <= 100) {
+          setAni(!ani)
+          return x + 10
+        }
+        else {
+          return x - 10
+        }
+      }
+
+    }
+  }
+
+  const layers = [
+    new ScatterplotLayer({
+      id: 'line-layer',
+      data,
+      opacity: .6,
+      pickable: true,
+      stroked: true,
+      filled: true,
+      radiusScale: 2,
+      radiusMinPixels: 1,
+      lineWidthMinPixels: 1,
+      getPosition: d => d.coordinates,
+      getRadius: d => Math.sqrt(d.exits),
+      getFillColor: d => d.color,
+      getLineColor: d => d.color,
+    })
+  ]
+
+  const layers2 = [
+    new ScatterplotLayer({
+      id: 'line-layer-2',
+      data,
+      opacity: .2,
+      pickable: true,
+      stroked: true,
+      filled: true,
+      radiusScale: 2,
+      radiusMinPixels: 1,
+      lineWidthMinPixels: 1,
+      getPosition: d => d.coordinates,
+      getRadius: d => 32,
+      getFillColor: d => d.color,
+      getLineColor: d => d.color,
+    })
+  ]
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      cambio()
+    }, 5);
+    return () => clearInterval(interval);
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <DeckGL
+      initialViewState={viewState}
+      controller={true}
+      ContextProvider={MapContext.Provider}
+      layers={[layers2, layers]}
+    >
+      <ScatterplotLayer id="scatter" data={data} getFillColor={[255, 255, 255]} />
+      <StaticMap
+        mapboxApiAccessToken={process.env.REACT_APP_KEY_MAPBOX}
+        mapStyle={process.env.REACT_APP_STYLE_MAPBOX}
+      >
+      </StaticMap>
+    </DeckGL>)
 }
+
 
 export default App;
